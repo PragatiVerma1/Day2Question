de3e3c6ac113e97a558ddf4030b08ad457192bff
unknown <Pragati1.Verma@lntinfotech.com>
2020-12-16 16:44:51 +0530

    CS Proj PrimeNumber

---
+++ Day2Question/Day2Question/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Question
+{
+    class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start number and end number");
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Prime Numbers between {min} and {max} are");
+            for (int i = min; i <= max; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0 && i != 1)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
